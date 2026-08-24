3c93b5f6f55b49207b124010f5bdfc5e2eec1e9b
adhamsabryartist-prog <adhamsabry.artist@gmail.com>
2026-08-24 13:10:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/',
+    base: 'https://adhamsabryartist-prog.github.io/Khamin-Takhmina-Static/',
     plugins: [
       react(), 
       tailwindcss(),
