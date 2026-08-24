a0fdc9add52f19cd9e670a1e7cb2b5dade671232
adhamsabryartist-prog <adhamsabry.artist@gmail.com>
2026-08-24 11:23:15 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,85 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/',
+    plugins: [
+      react(), 
+      tailwindcss(),
+      VitePWA({
+        strategies: 'injectManifest',
+        srcDir: 'src',
+        filename: 'sw.ts',
+        registerType: 'prompt',
+        injectRegister: 'auto',
+        injectManifest: {
+          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        },
+        manifest: {
+          id: '/',
+          name: 'خمن تخمينة',
+          short_name:'خمن تخمينة',
+          description: 'منصة خمن تخمينة للألعاب الجماعية أونلاين! تضم أكثر من 13 لعبة ممتعة مثل تخمين الصور، سباق التخمين، بازل، ووردل، وأتوبيس كومبليت. العب وتحدّ أصدقائك الآن مع ألعاب جديدة باستمرار.',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          display: 'standalone', // خليها standalone أحسن للتوافق
+          orientation: 'portrait',
+          icons: [
+            {
+              src: 'icon-3.png',
+              sizes: '144x144', // الـ SVG يفضل يكون any
+              type: 'image/png',
+              purpose: 'any' 
+            },
+            {
+              src: 'icon-192.png', // لازم تضيف نسخة PNG
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'icon-512.png', // لازم تضيف نسخة PNG
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable' // عشان تملأ الأيقونة في أندرويد
+            }
+          ],
+          screenshots: [
+            {
+              src: 'screenshot-mobile.png',
+              sizes: '1080x1920', // اتأكد من مقاس الصورة اللي صورتها
+              type: 'image/png',
+              form_factor: 'narrow',
+              label: 'Game Play on Mobile'
+            },
+            {
+              src: 'screenshot-desktop.png',
+              sizes: '1920x1080', // اتأكد من مقاس الصورة اللي صورتها
+              type: 'image/png',
+              form_factor: 'wide',
+              label: 'Game Play on Desktop'
+            }
+          ]
+        }
+      })
+    ],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      // HMR is disabled in AI Studio via DISABLE_HMR env var.
+      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
+      hmr: process.env.DISABLE_HMR !== 'true',
+    },
+  };
+});
