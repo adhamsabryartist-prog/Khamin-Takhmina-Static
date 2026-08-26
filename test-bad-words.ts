25d6866530c64ce37a59304944f55f70b8ae02e4
adhamsabryartist-prog <adhamsabry.artist@gmail.com>
2026-08-26 18:19:33 +0300

    Delete test-bad-words.ts

--- test-bad-words.ts
+++
@@ -1,7 +0,0 @@
-import { Filter } from 'bad-words';
-try {
-  const filter = new Filter();
-  console.log("Filter created successfully");
-} catch (e) {
-  console.error("Error creating filter:", e);
-}
