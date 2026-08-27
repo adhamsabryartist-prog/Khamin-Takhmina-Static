a4ad8614ef4bb1b6fa8247636ae4fc34883d60c7
adhamsabryartist-prog <adhamsabry.artist@gmail.com>
2026-08-27 17:25:38 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Khamin-Takhmina-Static/',
+    base: './',
     plugins: [
       react(), 
       tailwindcss(),
